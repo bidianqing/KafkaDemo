34ee782d6a9125bbebedc49ae65a33752e5f3ad3
毕殿卿 <bdq123@outlook.com>
2018-07-26 00:54:39 +0800

    添加项目文件。

---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,30 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KafkaProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new Dictionary<string, object>
+            {
+                { "bootstrap.servers", "192.168.199.235:9092" }
+            };
+
+            using (var producer = new Producer<Null, string>(config, null, new StringSerializer(Encoding.UTF8)))
+            {
+                while (true)
+                {
+                    Console.WriteLine("按任意键发送消息");
+                    Console.ReadKey();
+                    var dr = producer.ProduceAsync("my-topic", null, "test message text").Result;
+                    Console.WriteLine($"Delivered '{dr.Value}' to: {dr.TopicPartitionOffset}");
+                }
+            }
+        }
+    }
+}
